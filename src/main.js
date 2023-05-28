6d728009a36b5d1e65d7d9ea823e2d10efc6ceaf
utrodus <contact.utrodus@gmail.com>
2023-05-28 16:47:40 +0700

    fix: :bug: error when build site

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 // Import main css
 import "~/assets/style/index.scss";
-import Tawk from '@tawk.to/tawk-messenger-vue-3'
 
 // Import default layout so we don't need to import it to every page
 import DefaultLayout from "~/layouts/Default.vue";
@@ -8,10 +7,13 @@ import DefaultLayout from "~/layouts/Default.vue";
 export default function(Vue, { head }) {
 	// Set default layout as a global component
 	Vue.component("Layout", DefaultLayout);
+	if (process.isClient) {
+		const Tawk = require("@tawk.to/tawk-messenger-vue-3").default;
 
 	 // Register the Tawk component
 	 Vue.use(Tawk, {
 		 propertyId : '6472a32774285f0ec46e01c4',
     	 widgetId : '1h1fsqktn'
 	  })
+}
 }
